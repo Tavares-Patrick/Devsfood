92118887077dd09c498628b9a70b4f76e7890b38
Patrick Tavares <patrick.t2@hotmail.com>
2025-05-08 16:42:42 -0300

    first commit

---
+++ src/AppStyled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    height: 100vh;
+    background-color: red;
+`;
+
+export const Menu = styled.div`
+    display: flex;
+    background-color: #136713;
+    width: 80px;
+    flex-direction: column;
+`;
+
+export const PageBody = styled.div`
+    display: flex;
+    background-color: blue;
+    flex: 1;
+`;
